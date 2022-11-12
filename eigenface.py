9dbe09939a82c4d224a0d27a7060e89c486bbe6c
maikeljh <mjh191003@gmail.com>
2022-11-12 21:42:35 +0700

    add final step to compare and get closest image

--- eigenface.py
+++ eigenface.py
@@ -47,7 +47,6 @@ for i in range(len(listOfMatrixFace)):
 # Finding the covariance matrix by multiplying A Transpose with A
 covariance = np.matmul(difference,np.transpose(difference)) # In this case, difference is in form of A Transpose
 
-#covariance = np.matrix(covariance)
 #with open('outfile.txt','wb') as f:
     #for line in covariance:
         #np.savetxt(f, line, fmt='%.2f')

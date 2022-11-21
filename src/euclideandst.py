841a19e608b28e295047122fcf0879ee3afe8576
maikeljh <mjh191003@gmail.com>
2022-11-21 12:12:29 +0700

    add header for module euclidean distance

--- src/euclideandst.py
+++ src/euclideandst.py
@@ -1,7 +1,16 @@
 import numpy as np
 
 def euclideandst(vektor1, vektor2):
-#vektor 1 sama 2 ukurannya sama kan ya
+    """
+    Function to calculate euclidean distance between two vectors
+
+    Args:
+        vektor1 (array): vector 1 (same size)
+        vektor2 (array): vector 2 (same size)
+
+    Returns:
+        sum (float): the euclidean distance
+    """
     sum = 0
     for i in range(len(vektor1)):
         sum += ((vektor1[i] - vektor2[i]) * (vektor1[i] - vektor2[i]))
@@ -9,7 +18,17 @@ def euclideandst(vektor1, vektor2):
     return sum
 
 def shortestDst(vektor, matriksVektor):
-#matriksVektor si jumlah kolomnya sama kek vektor 2 kan ya
+    """
+    Function to determine shortest euclidean distance
+
+    Args:
+        vektor (array): the vector weight of test image
+        matriksVektor (matrix): the list of vector weight of training images
+
+    Returns:
+        idx (int): the index of the closest image
+        cachemin (float) : the shortest euclidean distance
+    """
     cachemin = euclideandst(vektor, matriksVektor[0])
     idx = 0
     for i in range(1, len(matriksVektor)):
